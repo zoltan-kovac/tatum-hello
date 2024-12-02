7f6d59b531eac633c77754fe2f5a6d68070e8b1c
Zoltan Kovac <zoltan.kovac@outlook.com>
2024-12-02 17:49:48 +0100

    add biome check

--- src/pages/Home/form.tsx
+++ src/pages/Home/form.tsx
@@ -1,12 +1,14 @@
 // App.js
-import { Network, TatumSDK, Ethereum } from "@tatumio/tatum";
-import React, { useState } from "react";
+import { type Ethereum, Network, TatumSDK } from "@tatumio/tatum";
+import type React from "react";
+import { useState } from "react";
 
 function Form() {
   const [inputValue, setInputValue] = useState(""); // State to hold the input value
   const [labelText, setLabelText] = useState(""); // State to hold the label text
 
-  const handleButtonClick = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const tatum = await TatumSDK.init<Ethereum>({
       network: Network.ETHEREUM,
       apiKey: { v4: import.meta.env.VITE_TATUM_MAINNET_API_KEY },
@@ -16,7 +18,7 @@ function Form() {
       addresses: [inputValue],
     });
     const balanceData = balance.data.filter(
-      (asset) => asset.asset === "ETH"
+      (asset) => asset.asset === "ETH",
     )[0];
 
     setLabelText(`Balance: ${balanceData.balance}`);
@@ -24,18 +26,22 @@ function Form() {
 
   return (
     <div>
-      <p>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          placeholder="Enter ETH wallet address to get balance"
-          style={{ padding: "5px", width: "320px" }}
-        />
-      </p>
-      <button onClick={handleButtonClick} style={{ padding: "5px" }}>
-        Click Me
-      </button>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setInputValue(e.currentTarget.value)
+            }
+            placeholder="Enter ETH wallet address to get balance"
+            style={{ padding: "5px", width: "320px" }}
+          />
+        </p>
+        <button type="submit" style={{ padding: "5px" }}>
+          Click Me
+        </button>
+      </form>
       <p style={{ padding: "5px", fontSize: "16px", fontWeight: "bold" }}>
         {labelText}
       </p>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,15 @@
-import preactLogo from '../../assets/tatum.jpeg'
-import Form from './form';
-import './style.css';
+import preactLogo from "../../assets/tatum.jpeg";
+import Form from "./form";
+import "./style.css";
 
 export function Home() {
-	return (
-		<div class="home">
-			<a href="https://preactjs.com" target="_blank">
-				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
-			</a>
-			<h1>Tatum Hello</h1>
-			{/* <section>
+  return (
+    <div class="home">
+      <a href="https://preactjs.com" target="_blank" rel="noreferrer">
+        <img src={preactLogo} alt="Preact logo" height="160" width="160" />
+      </a>
+      <h1>Tatum Hello</h1>
+      {/* <section>
 				<Resource
 					title="Learn Preact"
 					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
@@ -26,16 +26,16 @@ export function Home() {
 					href="https://vitejs.dev"
 				/>
 			</section> */}
-			<Form />
-		</div>
-	);
+      <Form />
+    </div>
+  );
 }
 
 function Resource(props) {
-	return (
-		<a href={props.href} target="_blank" class="resource">
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
-	);
+  return (
+    <a href={props.href} target="_blank" class="resource" rel="noreferrer">
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </a>
+  );
 }
